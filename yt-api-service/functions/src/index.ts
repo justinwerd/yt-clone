ed1959d3b898fc50123bbfa0732850317b8b3b04
Justin Dieffenbach <justinwerd@gmail.com>
2025-09-16 09:25:11 -0400

    firebase oncreate trial

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,32 +1,21 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// import * as functions from "firebase-functions/v2";
+import {beforeUserCreated} from "firebase-functions/v2/identity";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
 
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+initializeApp();
+const firestore = new Firestore();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const onUserCreate = beforeUserCreated(async (event) => {
+    const user = event.data;
+    const userInfo = {
+        uid: user.uid,
+        email: user.email,
+        photoUrl: user.photoURL,
+      };
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+    console.log(`User Created: ${JSON.stringify(user)}`);
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // Example: you can write extra info, send welcome email, etc.
+    await firestore.collection("users").doc(user.uid).set(userInfo);
+  });
